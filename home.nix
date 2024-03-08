16358613ed624a7e9943500f27b76cbc8d3183b4
Ibrahim Dursun <ibrahim@dursun.cc>
2024-03-08 10:36:48 +0000

    Enable autosquash

--- home.nix
+++ home.nix
@@ -70,6 +70,7 @@ in
       };
       merge.tool = "code";
       mergetool."code".cmd = "code --wait --merge $REMOTE $LOCAL $BASE $MERGED";
+      rebase.autosquash = true;
     };
 
     includes = [
