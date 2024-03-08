a7e51636005f67a0d4dbda17d5f223bb3ccc20d4
Ibrahim Dursun <ibrahim@dursun.cc>
2024-03-08 10:32:43 +0000

    Disable coc

--- home.nix
+++ home.nix
@@ -152,23 +152,6 @@ in
       vim-multiple-cursors
       vim-highlightedyank
     ];
-    coc = {
-      enable = true;
-      pluginConfig = ''
-        " Use tab for trigger completion with characters ahead and navigate.
-        " Use command ':verbose imap <tab>' to make sure tab is not mapped by other plugin.
-        inoremap <silent><expr> <TAB>
-              \ pumvisible() ? "\<C-n>" :
-              \ <SID>check_back_space() ? "\<TAB>" :
-              \ coc#refresh()
-        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-
-        function! s:check_back_space() abort
-          let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-        endfunction
-      '';
-    };
     extraConfig = ''
       set ignorecase
       set tabstop=4
