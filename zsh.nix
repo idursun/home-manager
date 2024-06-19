ac814abf66420a88e18cd5c7c2b6b3c135fe6546
Ibrahim Dursun <ibrahim@dursun.cc>
2024-06-19 10:41:30 +0100

    feat(zsh): Add edit-command-line

--- zsh.nix
+++ zsh.nix
@@ -4,13 +4,19 @@
     history = {
       ignoreDups = true;
     };
-    defaultKeymap = "emacs";
+    defaultKeymap = "viins";
     enableCompletion = true;
     enableAutosuggestions = true;
-    syntaxHighlighting = {
-      enable = true;
-    };
+    syntaxHighlighting.enable = true; 
     initExtra = ''
+      bindkey  "^[[H"   beginning-of-line
+      bindkey  "^[[F"   end-of-line
+      bindkey  "^[[3~"  delete-char
+      bindkey ^R history-incremental-search-backward 
+      bindkey ^S history-incremental-search-forward
+
+      autoload edit-command-line; zle -N edit-command-line
+      bindkey -M vicmd v edit-command-line
       export COLORTERM=truecolor
       source <(kubectl completion zsh)
       source $HOME/.dev-local-setup
