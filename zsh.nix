5a84f6ade68ed9bb1cac0a85927d61638d8a0752
Ibrahim Dursun <ibrahim@dursun.cc>
2024-06-28 17:30:44 +0100

    feat(zsh): open command in editor

--- zsh.nix
+++ zsh.nix
@@ -4,19 +4,18 @@
     history = {
       ignoreDups = true;
     };
-    defaultKeymap = "viins";
+    defaultKeymap = "emacs";
     enableCompletion = true;
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true; 
     initExtra = ''
+      autoload edit-command-line; zle -N edit-command-line
+
       bindkey  "^[[H"   beginning-of-line
       bindkey  "^[[F"   end-of-line
       bindkey  "^[[3~"  delete-char
-      bindkey ^R history-incremental-search-backward 
-      bindkey ^S history-incremental-search-forward
+      bindkey  "^X^E"   edit-command-line
 
-      autoload edit-command-line; zle -N edit-command-line
-      bindkey -M vicmd v edit-command-line
       export COLORTERM=truecolor
       source <(kubectl completion zsh)
       source $HOME/.dev-local-setup
