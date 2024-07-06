cd044fb03ab02ceb3f2db012765deb9009f356a5
ibrahim dursun <idursun@stackoverflow.com>
2024-07-06 16:21:39 +0100

    remap find file to leader ff

--- neovim/neovim.nix
+++ neovim/neovim.nix
@@ -25,7 +25,7 @@
         config = ''
           local builtin = require('telescope.builtin')
 
-          vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+          vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
           vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
           vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         '';
