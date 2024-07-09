e34c82ec9a89d97af6514a44791d00953f9f71a0
ibrahim dursun <ibrahim@dursun.cc>
2024-07-09 19:11:34 +0100

    feat(nvim): add configuration for netrw, so that it looks more modern

--- neovim/remap.lua
+++ neovim/remap.lua
@@ -11,7 +11,13 @@ vim.opt.incsearch = true
 vim.opt.ttimeout = true
 vim.opt.ttimeoutlen = 100
 vim.opt.smarttab = true
+vim.opt.autochdir = true
 vim.wo.relativenumber = true
+
+-- Netrw configuration
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = false
+vim.g.netrw_winsize = 25
 
 vim.cmd [[ colorscheme dracula ]]
 vim.keymap.set('n', "<C-f>", "<C-f>zz", { silent = true })
