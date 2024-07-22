823e2fdfa47ea2c99c5363f28931ecaedb5f28b3
ibrahim dursun <ibrahim@dursun.cc>
2024-07-22 23:08:51 +0100

    move last buffer keybinding to remap

--- neovim/neovim.nix
+++ neovim/neovim.nix
@@ -112,7 +112,6 @@
           ]]
           vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
           vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
-          vim.keymap.set('n', '<leader>b', ':b#<CR>')
         '';
       }
     ];
