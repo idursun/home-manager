e1973c58598b11c25a2b59b96372d9776ddaff8d
ibrahim dursun <ibrahim@dursun.cc>
2024-09-09 09:29:45 +0100

    remove neovim configuration from home-manager
    I moved my neovim-configuration to `idursun/nvim-config` repository

--- neovim.nix
+++ neovim.nix
@@ -1,18 +1,5 @@
-{ pkgs, lib, ... }:
-let
-  nvimConfig = pkgs.fetchFromGitHub {
-    owner = "idursun";
-    repo = "nvim-config";
-    rev = "main"; 
-    sha256 = "sha256-nAIgx/yqBhnH0JgeCGSrYA93wMGDZqfBXma9DTMtAB0=";
-  };
-in
+{ pkgs, ... }:
 {
-  home.file.".config/nvim" = {
-    source = nvimConfig;
-    recursive = true;
-  };
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;
