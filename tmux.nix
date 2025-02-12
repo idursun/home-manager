9c7b24df4a7f50f311d158ed546601df122b5832
ibrahim dursun <ibrahim@dursun.cc>
2025-02-12 00:50:55 +0000

    refactor(tmux): use built-in configuration

--- tmux.nix
+++ tmux.nix
@@ -4,13 +4,12 @@
     enable = true;
     keyMode = "vi";
     mouse = true;
-    shortcut = "b";
     terminal = "screen-256color";
+    escapeTime = 0;
     plugins = with pkgs; [
       { plugin = tmuxPlugins.dracula;
         extraConfig = ''
           set -g @dracula-plugins "none"
-          set -s escape-time 0
         '';
       }
     ];
