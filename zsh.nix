380b799bfd628929b4a5cb65f14c625912d24c09
ibrahim dursun <ibrahim@dursun.cc>
2025-02-12 01:15:39 +0000

    feat(zsh): change cursor shape based on keymap

--- zsh.nix
+++ zsh.nix
@@ -4,17 +4,19 @@
     history = {
       ignoreDups = true;
     };
-    defaultKeymap = "emacs";
     enableCompletion = true;
     autosuggestion.enable = true;
-    syntaxHighlighting.enable = true; 
+    syntaxHighlighting.enable = true;
+    sessionVariables = {
+      KEYTIMEOUT = 1;
+    };
     initExtra = ''
       autoload edit-command-line; zle -N edit-command-line
-
-      bindkey  "^[[H"   beginning-of-line
-      bindkey  "^[[F"   end-of-line
-      bindkey  "^[[3~"  delete-char
       bindkey  "^X^E"   edit-command-line
+      bindkey "^?" backward-delete-char
+      bindkey "^W" backward-delete-word
+
+      ${builtins.readFile ./zle-keymap-select.zsh}
 
       export COLORTERM=truecolor
       export PATH=$PATH:/Users/idursun/.cargo/bin
