24a0e8f299751cccbf575f69837f9ce008dffc7d
ibrahim dursun <ibrahim@dursun.cc>
2025-03-05 11:30:32 +0000

    configure fish

--- bare/fish/config.fish
+++ bare/fish/config.fish
@@ -1,6 +1,48 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set EDITOR nvim
 end
 
-fish_add_path -gP "/opt/homebrew/bin" "/opt/homebrew/sbin" "$HOME/go/bin"
+set -gx EDITOR nvim
+
+fish_add_path -gP "/usr/local/bin" "/opt/homebrew/bin" "/opt/homebrew/sbin" "$HOME/go/bin"
+
+# Colorscheme: ayu Mirage
+set -U fish_color_normal CBCCC6
+set -U fish_color_command 5CCFE6
+set -U fish_color_keyword 5CCFE6
+set -U fish_color_quote BAE67E
+set -U fish_color_redirection D4BFFF
+set -U fish_color_end F29E74
+set -U fish_color_error FF3333
+set -U fish_color_param CBCCC6
+set -U fish_color_comment 5C6773
+set -U fish_color_match F28779
+set -U fish_color_selection --background=FFCC66
+set -U fish_color_search_match --background=FFCC66
+set -U fish_color_history_current --bold
+set -U fish_color_operator FFCC66
+set -U fish_color_escape 95E6CB
+set -U fish_color_cwd 73D0FF
+set -U fish_color_cwd_root red
+set -U fish_color_option CBCCC6
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 707A8C
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_history_current --bold
+set -U fish_color_status red
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=FFCC66
+set -U fish_pager_color_background
+set -U fish_pager_color_selected_description
+set -U fish_pager_color_selected_prefix
+set -U fish_pager_color_selected_completion
+set -U fish_pager_color_secondary_prefix
+set -U fish_pager_color_secondary_description
+set -U fish_pager_color_secondary_completion
+set -U fish_pager_color_secondary_background
--- bare/fish/configure_fish.sh
+++ bare/fish/configure_fish.sh
@@ -2,3 +2,4 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 cp "$SCRIPT_DIR/config.fish" "$HOME/.config/fish/"
+cp "$SCRIPT_DIR/functions/fish_prompt.fish" "$HOME/.config/fish/functions/"
