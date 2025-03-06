420a370eaa39fe31198c3c2ef61c0862a742b095
ibrahim dursun <ibrahim@dursun.cc>
2025-03-06 13:36:23 +0000

    feat(git): add allowed_signers file

--- bare/git/configure_git.sh
+++ bare/git/configure_git.sh
@@ -5,3 +5,5 @@ mkdir -p $HOME/.config/git
 cp "$SCRIPT_DIR/git_include_repositories" "$HOME/.config/git/"
 cp "$SCRIPT_DIR/git_include_workspace" "$HOME/.config/git/"
 cp "$SCRIPT_DIR/config" "$HOME/.config/git/config"
+
+cp "$SCRIPT_DIR/allowed_signers" "$HOME/.ssh/allowed_signers"
