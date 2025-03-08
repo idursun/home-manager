3282ebabe528f48849427263e6aadbbb6064b0d8
ibrahim dursun <ibrahim@dursun.cc>
2025-03-08 20:04:36 +0000

    feat(fish): change theme to dracula

--- bare/fish/config.fish
+++ bare/fish/config.fish
@@ -10,42 +10,42 @@ end
 
 bind -M insert \cr history-pager
 
-# Colorscheme: ayu Mirage
-set -U fish_color_normal CBCCC6
-set -U fish_color_command 5CCFE6
-set -U fish_color_keyword 5CCFE6
-set -U fish_color_quote BAE67E
-set -U fish_color_redirection D4BFFF
-set -U fish_color_end F29E74
-set -U fish_color_error FF3333
-set -U fish_color_param CBCCC6
-set -U fish_color_comment 5C6773
-set -U fish_color_match F28779
-set -U fish_color_selection --background=FFCC66
-set -U fish_color_search_match --background=FFCC66
+# Colorscheme: Dracula
+set -U fish_color_normal f8f8f2
+set -U fish_color_command 8be9fd
+set -U fish_color_keyword ff79c6
+set -U fish_color_quote f1fa8c
+set -U fish_color_redirection f8f8f2
+set -U fish_color_end ffb86c
+set -U fish_color_error ff5555
+set -U fish_color_param bd93f9
+set -U fish_color_comment 6272a4
+set -U fish_color_match --background=brblue
+set -U fish_color_selection --background=44475a
+set -U fish_color_search_match --background=44475a
 set -U fish_color_history_current --bold
-set -U fish_color_operator FFCC66
-set -U fish_color_escape 95E6CB
-set -U fish_color_cwd 73D0FF
+set -U fish_color_operator 50fa7b
+set -U fish_color_escape ff79c6
+set -U fish_color_cwd 50fa7b
 set -U fish_color_cwd_root red
-set -U fish_color_option CBCCC6
+set -U fish_color_option ffb86c
 set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion 707A8C
-set -U fish_color_user brgreen
-set -U fish_color_host normal
-set -U fish_color_host_remote yellow
+set -U fish_color_autosuggestion 6272a4
+set -U fish_color_user 8be9fd
+set -U fish_color_host bd93f9
+set -U fish_color_host_remote bd93f9
 set -U fish_color_history_current --bold
-set -U fish_color_status red
-set -U fish_color_cancel --reverse
-set -U fish_pager_color_prefix normal --bold --underline
-set -U fish_pager_color_progress brwhite --background=cyan
-set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D
-set -U fish_pager_color_selected_background --background=FFCC66
+set -U fish_color_status ff5555
+set -U fish_color_cancel ff5555 --reverse
 set -U fish_pager_color_background
-set -U fish_pager_color_selected_description
-set -U fish_pager_color_selected_prefix
-set -U fish_pager_color_selected_completion
+set -U fish_pager_color_prefix 8be9fd
+set -U fish_pager_color_progress 6272a4
+set -U fish_pager_color_completion f8f8f2
+set -U fish_pager_color_description 6272a4
+set -U fish_pager_color_selected_background --background=44475a
+set -U fish_pager_color_selected_prefix 8be9fd
+set -U fish_pager_color_selected_completion f8f8f2
+set -U fish_pager_color_selected_description 6272a4
 set -U fish_pager_color_secondary_prefix
 set -U fish_pager_color_secondary_description
 set -U fish_pager_color_secondary_completion
